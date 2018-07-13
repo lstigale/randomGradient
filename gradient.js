acefdcc03ab61b2d46fd477696993a94eee15515
lstigale <leahstigale@gmail.com>
2018-07-13 11:23:10 -0700

    Made it so it runs every refresh

--- gradient.js
+++ gradient.js
@@ -1,12 +1,12 @@
-function randomColor() {
-  var o = Math.floor( Math.random() * 360 );
-  var x = Math.floor( Math.random() * 256 );
-  var y = Math.floor( Math.random() * 256 );
-  var z = Math.floor( Math.random() * 256 );
-  var x2 = Math.floor( Math.random() * 256 );
-  var y2 = Math.floor( Math.random() * 256 );
-  var z2 = Math.floor( Math.random() * 256 );
-  console.log(o, x,y,z, x2,y2,z2);
-document.body.style.background = "linear-gradient(" + o + "deg, rgb("+ x + ", " + y + ", " + z + "), rgb("+ x2 + ", " + y2 + ", " + z2 + "))";
-document.getElementById('colorNum').innerHTML = "linear-gradient(" + o + "deg, rgb(" + x + ", " + y + ", " + z + "), rgb("+ x2 + ", " + y2 + ", " + z2 + "));";
+function randomGradient() {
+  var deg = Math.floor( Math.random() * 360 );
+  var r = Math.floor( Math.random() * 256 );
+  var g = Math.floor( Math.random() * 256 );
+  var b = Math.floor( Math.random() * 256 );
+  var r2 = Math.floor( Math.random() * 256 );
+  var g2 = Math.floor( Math.random() * 256 );
+  var b2 = Math.floor( Math.random() * 256 );
+  console.log(deg, r,g,b, r2,g2,b2);
+document.body.style.background = "linear-gradient(" + deg + "deg, rgb("+ r + ", " + g + ", " + b + "), rgb("+ r2 + ", " + g2 + ", " + b2 + "))";
+document.getElementById('colorNum').innerHTML = "linear-gradient(" + deg + "deg, rgb(" + r + ", " + g + ", " + b + "), rgb("+ r2 + ", " + g2 + ", " + b2 + "));";
 }
